37b877a794585a2dd4ba5ac909ab6214650999ed
Dyana Rahhal <dyanarahhal98@gmail.com>
2026-04-25 02:41:00 -0500

    setup Spring Boot backend project

---
+++ backend/thoughtfull-api/src/main/java/com/thoughtfull/api/thoughtfull_api/ThoughtfullApiApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtfull.api.thoughtfull_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtfullApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoughtfullApiApplication.class, args);
+	}
+
+}
