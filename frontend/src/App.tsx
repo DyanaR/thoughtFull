821ba94110ca1b46057e165eaef2cf1bdb50e39b
Dyana Rahhal <dyanarahhal98@gmail.com>
2026-06-03 14:29:12 -0500

    feat: connect React frontend to Auth0 and backend user endpoint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,122 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import { getCurrentUser } from "./services/api";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // loginWithRedirect: redirects user to Auth0 login page
+  // logout: logs user out and redirects back to app
+  // isAuthenticated: true if user is logged in, false otherwise
+  // user: object containing logged-in user ingo (e.g., name, email)
+  const {
+    loginWithRedirect,
+    logout,
+    isAuthenticated,
+    user,
+    getAccessTokenSilently,
+  } = useAuth0();
+
+  const [backendUser, setBackendUser] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (!isAuthenticated) return;
+
+      const token = await getAccessTokenSilently();
+
+      const user = await getCurrentUser(token);
+      setBackendUser(user);
+      console.log(user);
+    };
+    fetchUser();
+  }, [isAuthenticated]);
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <div>
+        <h1>ThoughtFull</h1>
 
-      <div className="ticks"></div>
+        {/* user not logged in */}
+        {!isAuthenticated && (
+          <button onClick={() => loginWithRedirect()}>Login</button>
+        )}
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        {/* user logged in */}
+        {isAuthenticated && (
+          <>
+            <h1>Welcome {user?.name}</h1>
+            <button
+              onClick={() =>
+                logout({ logoutParams: { returnTo: window.location.origin } })
+              }
+            >
+              Logout
+            </button>
+          </>
+        )}
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+        {backendUser && <p>Backend user: {backendUser.name}</p>}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
