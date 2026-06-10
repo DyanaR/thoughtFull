fd1c9d4fdec04b96cf0a808af3dc446c8df52fec
Dyana Rahhal <dyanarahhal98@gmail.com>
2026-06-09 23:31:31 -0500

    feat: add Patch api for entry

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -50,3 +50,52 @@ export async function createEntry(token: string, request: CreateEntryRequest) {
 
   return response.json();
 }
+
+// Get a Entry by ID
+export async function getEntryById(token: string, entryId: string) {
+  const response = await fetch(
+    `http://localhost:8080/api/v1/entries/${entryId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch entry. Status: ${response.status}`);
+  }
+
+  return response.json();
+}
+
+// Edit an Entry by ID
+type UpdateEntryRequest = {
+  title?: string;
+  content?: string;
+  moodIds?: string[];
+};
+
+export async function updateEntry(
+  token: string,
+  entryId: string,
+  request: UpdateEntryRequest,
+) {
+  const response = await fetch(
+    `http://localhost:8080/api/v1/entries/${entryId}`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(request),
+    },
+  );
+
+  if (!response.ok) {
+    throw new Error(`Failed to update entry. Status: ${response.status}`);
+  }
+
+  return response.json();
+}
