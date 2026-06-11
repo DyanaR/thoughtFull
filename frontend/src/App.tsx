a64178237ff0d07ef187fd82453cad8993cffb18
Dyana Rahhal <dyanarahhal98@gmail.com>
2026-06-10 22:41:15 -0500

    feat: add required name input from user

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,70 @@
 import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
 import HomePage from "./pages/HomePage";
 import EntryPage from "./pages/EntryPage";
+import MoodSelectionPage from "./pages/MoodSelectionPage";
+import AddEntryPage from "./pages/AddEntryPage";
+import LandingPage from "./pages/LandingPage";
+import NamePage from "./pages/NamePage";
+import AuthRedirectPage from "./pages/AuthRedirectPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/entries/:id" element={<EntryPage />} />
+      {/* public pages */}
+      <Route path="/" element={<LandingPage />} />
+
+      {/* protected pages */}
+      <Route
+        path="/auth-redirect"
+        element={
+          <ProtectedRoute>
+            <AuthRedirectPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/name"
+        element={
+          <ProtectedRoute>
+            <NamePage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/entries/:id"
+        element={
+          <ProtectedRoute>
+            <EntryPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/moods"
+        element={
+          <ProtectedRoute>
+            <MoodSelectionPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/add-entry"
+        element={
+          <ProtectedRoute>
+            <AddEntryPage />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
